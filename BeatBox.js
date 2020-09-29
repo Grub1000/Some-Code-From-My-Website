9ec87729b8128c7d11cd79ce326fc35faff3e725
Grub1000 <63425672+Grub1000@users.noreply.github.com>
2020-09-29 05:24:58 -0700

    Update BeatBox.js

--- BeatBox.js
+++ BeatBox.js
@@ -40,3 +40,30 @@ function addToTempCount(arg, likes) {
     element.innerHTML = newNum
   }
 }
+function getCookie(name) {
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== "") {
+    const cookies = document.cookie.split(";");
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      // Does this cookie string begin with the name we want?
+      if (cookie.substring(0, name.length + 1) === name + "=") {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+  return cookieValue;
+}
+var csrftoken = getCookie("csrftoken");
+function PushData2() {
+  fetch("/beatbox/post/like/" + history.state.ID_Memory.join(), {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+      "X-CSRFToken": csrftoken,
+    }
+  })
+  return;
+}
+window.onbeforeunload = PushData2;
